db6f254b4a81c62129d895e3c5a57a42b3cd9519
Menesth <poilonez78@yahoo.fr>
2025-01-16 20:22:24 +0100

    update

--- Tests/LogRegTest.py
+++ Tests/LogRegTest.py
@@ -4,14 +4,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 sys.path.append(os.path.abspath("Desktop/BasicML"))
-from Scripts.LinearRegression import LinearRegressionGD
+from Scripts.LogisticRegression import LogRegressionGD
 
 SIGMA = 1.0
 
 X = np.arange(start=-5.0, stop=5.0, step=0.1).reshape(-1, 1)
 y = 2 * X + 1 + np.random.normal(loc=0, scale=SIGMA**2, size=X.shape)
 
-model = LinearRegressionGD(in_features=X.shape[-1], out_features=1)
+model = LogRegressionGD(in_features=X.shape[-1], out_features=1)
 model.train(X, y, lr = 1e-3, epochs = 20, eval_epoch = 2)
 
 yhat = model(X)
